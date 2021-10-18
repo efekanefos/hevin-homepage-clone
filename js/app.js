e830a9097155d20e9169c949eeccb7b5cf26afbd
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-10-18 11:36:09 +0300

    mainSlider

--- js/app.js
+++ js/app.js
@@ -39,3 +39,38 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 960px)");
 myFunction(x); // Call listener function at run time
 x.addListener(myFunction); // Attach listener function on state changes
+
+/* Slider Part */
+
+var slides = document.querySelector(".slider-items").children;
+var nextSlide = document.querySelector(".right-slide");
+var prevSlide = document.querySelector(".left-slide");
+var totalSlides = slides.length;
+var index = 0;
+
+nextSlide.onclick = function () {
+  next("next");
+};
+prevSlide.onclick = function () {
+  next("prev");
+};
+
+function next(direction) {
+  if (direction == "next") {
+    index++;
+    if (index == totalSlides) {
+      index = 0;
+    }
+  } else {
+    if (index == 0) {
+      index = totalSlides - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+}
