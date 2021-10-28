0da568f8e87040186146be5716d70e87189bd0ba
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-10-28 22:20:54 +0300

    cartPageAdded

--- categoriesPages/livingRoomFiles/products.js
+++ categoriesPages/livingRoomFiles/products.js
@@ -1,185 +1,185 @@
 const newArr = [
   {
     name: "Olivia Koltuk Takımı Yeşil",
-    image: "../img/oliviaNehirKoltuk1.jpg",
+    image: "../../img/oliviaNehirKoltuk1.jpg",
   },
   {
     name: "Janet Koltuk Takımı Krem",
-    image: "../img/janetKoltuk1.jpg",
+    image: "../../img/janetKoltuk1.jpg",
   },
   {
     name: "Atlanta Koltuk Takımı Turuncu",
-    image: "../img/atlantaKoltuk1.jpg",
+    image: "../../img/atlantaKoltuk1.jpg",
   },
   {
     name: "Lake Koltuk Takımı Turuncu",
-    image: "./livingRoomImg/lakeTuruncu.jpg",
+    image: "../livingRoomImg/lakeTuruncu.jpg",
   },
   {
     name: "Olivia Koltuk Takımı Pembe",
-    image: "./livingRoomImg/oliviaPembe.jpg",
+    image: "../livingRoomImg/oliviaPembe.jpg",
   },
   {
     name: "Verona Koltuk Takımı Gri",
-    image: "./livingRoomImg/veronaGri.jpg",
+    image: "../livingRoomImg/veronaGri.jpg",
   },
   {
     name: "Verona Koltuk Takımı Kahve",
-    image: "./livingRoomImg/veronaKahve.jpg",
+    image: "../livingRoomImg/veronaKahve.jpg",
   },
   {
     name: "Verona Koltuk Takımı Krem",
-    image: "./livingRoomImg/veronaKrem.jpg",
+    image: "../livingRoomImg/veronaKrem.jpg",
   },
   {
     name: "Elegance Koltuk Takımı Bordo",
-    image: "./livingRoomImg/eleganceBordo.jpg",
+    image: "../livingRoomImg/eleganceBordo.jpg",
   },
   {
     name: "Elegance Koltuk Takımı Turkuaz",
-    image: "./livingRoomImg/eleganceTurkuaz.jpg",
+    image: "../livingRoomImg/eleganceTurkuaz.jpg",
   },
   {
     name: "Elegance Koltuk Takımı Antrasit",
-    image: "./livingRoomImg/eleganceAntrasit.jpg",
+    image: "../livingRoomImg/eleganceAntrasit.jpg",
   },
   {
     name: "Rooney Koltuk Takımı Krem",
-    image: "./livingRoomImg/rooneyKrem.jpg",
+    image: "../livingRoomImg/rooneyKrem.jpg",
   },
   {
     name: "Rooney Koltuk Takımı Örme Kumaş",
-    image: "./livingRoomImg/rooneyÖrme.jpg",
+    image: "../livingRoomImg/rooneyÖrme.jpg",
   },
   {
     name: "Rooney Koltuk Takımı Gri",
-    image: "./livingRoomImg/rooneyGri.jpg",
+    image: "../livingRoomImg/rooneyGri.jpg",
   },
   {
     name: "Hera Koltuk Takımı Gül Kurusu",
-    image: "./livingRoomImg/heraGül.jpg",
+    image: "../livingRoomImg/heraGül.jpg",
   },
   /* 15 */
   {
     name: "Hera Koltuk Takımı Lacivert",
-    image: "./livingRoomImg/heraLacivert.jpg",
+    image: "../livingRoomImg/heraLacivert.jpg",
   },
   {
     name: "Hera Koltuk Takımı Bordo",
-    image: "./livingRoomImg/heraBordo.jpg",
+    image: "../livingRoomImg/heraBordo.jpg",
   },
   {
     name: "Lake Koltuk Takımı Lacivert",
-    image: "./livingRoomImg/lakeLacivert.jpg",
+    image: "../livingRoomImg/lakeLacivert.jpg",
   },
   {
     name: "Lake Koltuk Takımı Turuncu",
-    image: "./livingRoomImg/lakeTuruncu.jpg",
+    image: "../livingRoomImg/lakeTuruncu.jpg",
   },
   {
     name: "Salsa Koltuk Takımı Gri",
-    image: "./livingRoomImg/salsaGri.jpg",
+    image: "../livingRoomImg/salsaGri.jpg",
   },
   {
     name: "Salsa Koltuk Takımı Siyah",
-    image: "./livingRoomImg/salsaSiyah.jpg",
+    image: "../livingRoomImg/salsaSiyah.jpg",
   },
   {
     name: "Salsa Koltuk Takımı Yeşil",
-    image: "./livingRoomImg/salsaYeşil.jpg",
+    image: "../livingRoomImg/salsaYeşil.jpg",
   },
   {
     name: "Elit Koltuk Takımı",
-    image: "./livingRoomImg/elit.jpg",
+    image: "../livingRoomImg/elit.jpg",
   },
   {
     name: "Janet Koltuk Takımı Kahve",
-    image: "./livingRoomImg/janetKahve.jpg",
+    image: "../livingRoomImg/janetKahve.jpg",
   },
   {
     name: "Atlanta Koltuk Takımı Krem",
-    image: "./livingRoomImg/atlantaKrem.jpg",
+    image: "../livingRoomImg/atlantaKrem.jpg",
   },
   {
     name: "Lake Koltuk Takımı Yeşil",
-    image: "./livingRoomImg/lakeYeşil.jpg",
+    image: "../livingRoomImg/lakeYeşil.jpg",
   },
   {
     name: "Janet Koltuk Takımı Lacivert",
-    image: "./livingRoomImg/janetLacivert.jpg",
+    image: "../livingRoomImg/janetLacivert.jpg",
   },
   {
     name: "Olivia Koltuk Takımı Gri",
-    image: "./livingRoomImg/oliviaGri.jpg",
+    image: "../livingRoomImg/oliviaGri.jpg",
   },
   {
     name: "Atlanta Koltuk Takımı Gri",
-    image: "./livingRoomImg/atlantaGri.jpg",
+    image: "../livingRoomImg/atlantaGri.jpg",
   },
   {
     name: "İstanbul Koltuk Takımı Lacivert",
-    image: "./livingRoomImg/istanbulLacivert.jpg",
+    image: "../livingRoomImg/istanbulLacivert.jpg",
   },
   /* 30 */
   {
     name: "İstanbul Köşe Takımı Gri",
-    image: "./livingRoomImg/istanbulGri.jpg",
+    image: "../livingRoomImg/istanbulGri.jpg",
   },
   {
     name: "İstanbul Köşe Takımı Kahve",
-    image: "./livingRoomImg/istanbulKahve.jpg",
+    image: "../livingRoomImg/istanbulKahve.jpg",
   },
   {
     name: "Atlanta Koltuk Takımı Turuncu",
-    image: "./livingRoomImg/atlantaKrem.jpg",
+    image: "../livingRoomImg/atlantaKrem.jpg",
   },
   {
     name: "Isabel Köşe Takımı",
-    image: "./livingRoomImg/isabel.jpg",
+    image: "../livingRoomImg/isabel.jpg",
   },
   {
     name: "Olivia Koltuk Takımı Mavi",
-    image: "./livingRoomImg/oliviaMavi.jpg",
+    image: "../livingRoomImg/oliviaMavi.jpg",
   },
   {
     name: "Olivia Köşe Takımı Krem",
-    image: "./livingRoomImg/oliviaKrem.jpg",
+    image: "../livingRoomImg/oliviaKrem.jpg",
   },
   {
     name: "Olivia Köşe Takımı Kırık Beyaz",
-    image: "./livingRoomImg/oliviaKırıkBeyaz.jpg",
+    image: "../livingRoomImg/oliviaKırıkBeyaz.jpg",
   },
   {
     name: "Salsa Köşe Takımı Gri",
-    image: "./livingRoomImg/salsaGri.jpg",
+    image: "../livingRoomImg/salsaGri.jpg",
   },
   {
     name: "Salsa Köşe Takımı Açık Gri",
-    image: "./livingRoomImg/salsaAçıkGri.jpg",
+    image: "../livingRoomImg/salsaAçıkGri.jpg",
   },
   {
     name: "Salsa Köşe Takımı Bordo",
-    image: "./livingRoomImg/salsaBordo.jpg",
+    image: "../livingRoomImg/salsaBordo.jpg",
   },
   {
     name: "Olivia Köşe Takımı Kırık Beyaz",
-    image: "./livingRoomImg/oliviaKırıkBeyaz.jpg",
+    image: "../livingRoomImg/oliviaKırıkBeyaz.jpg",
   },
   {
     name: "Rooney Koltuk Takımı Krem",
-    image: "./livingRoomImg/rooneyKrem.jpg",
+    image: "../livingRoomImg/rooneyKrem.jpg",
   },
   {
     name: "Olivia Köşe Takımı Kırık Beyaz",
-    image: "./livingRoomImg/oliviaKırıkBeyaz.jpg",
+    image: "../livingRoomImg/oliviaKırıkBeyaz.jpg",
   },
   {
     name: "Salsa Köşe Takımı Gri",
-    image: "./livingRoomImg/salsaGri.jpg",
+    image: "../livingRoomImg/salsaGri.jpg",
   },
   {
     name: "Salsa Köşe Takımı Bordo",
-    image: "./livingRoomImg/salsaBordo.jpg",
+    image: "../livingRoomImg/salsaBordo.jpg",
   },
   /* 45 */
 ];
